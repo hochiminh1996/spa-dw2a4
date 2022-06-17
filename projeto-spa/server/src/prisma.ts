204d53132ebc77e91af810f75e686379458e6e72
Felippe Marques <61765712+hochiminh1996@users.noreply.github.com>
2022-06-17 20:42:56 -0300

    Delete projeto-spa directory

--- projeto-spa/server/src/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-
-//FAZENDO  INSERÇAÕ
-export const prisma = new PrismaClient({
-    log: ['query']
-});
--- projeto-spa/web/src/App.tsx
+++
@@ -1,6 +0,0 @@
-// PÁGINA QUE IRÁ CHAMAR TODOS OS COMPONENTES
-import { Widget } from "./components/Widget";
-
-export function App() {
-  return <Widget />
-}
