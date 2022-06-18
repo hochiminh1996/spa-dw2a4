8886d5435f10d917acdda9c8ff0422682f96f296
Felippe Marques <ambrsp@hotmail.com>
2022-06-18 02:19:56 -0300

    BIBLIOTECA CORS

--- projeto-spa/server/src/server.ts
+++ projeto-spa/server/src/server.ts
@@ -1,12 +1,13 @@
 import express from "express"
+import cors from "cors";
 import { routes } from "./routes";
 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());//verifica se há json na requisição
 app.use(routes);
-
 
 app.listen(3333, () => {
     console.log("HTTP SERVER RUNNING!!");
