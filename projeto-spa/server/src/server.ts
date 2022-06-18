3efc6f08c1b89b02d062a00adb3047814c39d328
Felippe Marques <ambrsp@hotmail.com>
2022-06-18 03:41:52 -0300

    Ajustes do back-end

--- projeto-spa/server/src/server.ts
+++ projeto-spa/server/src/server.ts
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());//verifica se há json na requisição
 app.use(routes);
 
-app.listen(3333, () => {
+
+app.listen(process.env.PORT || 3333, () => {
     console.log("HTTP SERVER RUNNING!!");
-})
+});
