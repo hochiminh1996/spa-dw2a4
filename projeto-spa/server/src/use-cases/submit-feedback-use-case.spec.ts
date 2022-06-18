45a7ec196f974616b99a4c2087658aeafb6954d2
Felippe Marques <ambrsp@hotmail.com>
2022-06-18 02:15:19 -0300

    Testes unitarios

---
+++ projeto-spa/server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -0,0 +1,57 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy }
+)
+
+describe("SubmitFeedbackUseCase", () => {
+    it("should be able to submit feedback", async () => {
+
+        await expect(submitFeedback.execute({
+            name: "Felippe imagem",
+            email: "teste@gmail.com",
+            type: "BUG",
+            comment: "A example",
+            screenshot: "data:image/png;base64, imagem",
+        })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalled();
+        expect(sendMailSpy).toHaveBeenCalled();
+    });
+
+    it("should not be able to submit feedback without type", async () => {
+
+        await expect(submitFeedback.execute({
+            name: "Felippe imagem",
+            email: "teste@gmail.com",
+            type: "",
+            comment: "A example",
+            screenshot: "data:image/png;base64, imagem",
+        })).rejects.toThrow();
+    });
+
+
+    it('should not be able to submit a feedback without comment', async () => {
+        await expect(submitFeedback.execute({
+            name: "Felippe imagem",
+            email: "teste@gmail.com",
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64',
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback with an invalid image', async () => {
+        await expect(submitFeedback.execute({
+            name: "Felippe imagem",
+            email: "teste@gmail.com",
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: '123.test',
+        })).rejects.toThrow();
+    });
+});
--- projeto-spa/server/src/use-cases/submit-feedback-use-case.ts
+++ projeto-spa/server/src/use-cases/submit-feedback-use-case.ts
@@ -13,12 +13,44 @@ export class SubmitFeedbackUseCase {
 
     constructor(
         private feedbacksRepository: FeedbacksRepository,
-        private mailAdapter : MailAdapter,
-    ){}
+        private mailAdapter: MailAdapter,
+    ) { }
 
 
     async execute(request: SubmitFeedbackUseCaseRequest) {
         const { name, email, type, comment, screenshot } = request;
+
+        if(!name) {
+            throw new Error('Type is required.');
+        }
+
+        if(!email) {
+            throw new Error('Type is required.');
+        }
+
+        if(!comment) {
+            throw new Error('Type is required.');
+        }
+
+
+        if(!comment) {
+            throw new Error('Type is required.');
+        }
+
+
+        if (!type) {
+            throw new Error('Type is required.');
+        }
+
+        if (!comment) {
+            throw new Error('Type is required.');
+        }
+
+
+        if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
+            throw new Error("Invalid screenshot form")
+        }
+
 
 
         await this.feedbacksRepository.create({
@@ -32,11 +64,11 @@ export class SubmitFeedbackUseCase {
 
         await this.mailAdapter.sendMail({
             subject: "Novo Feedback",
-            body : [
+            body: [
                 `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
                 `<p>Tipo do feedback: ${type}</p>`,
                 `<p>Comentário: ${comment}</p>`,
-                screenshot ? `<img src="${screenshot}" />`: ``,
+                screenshot ? `<img src="${screenshot}" />` : ``,
                 `</div>`
             ].join('\n')
         })
